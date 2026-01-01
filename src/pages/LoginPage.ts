40fe1bc42ab8a89699ed21fba862cb1deb13deb3
darnelljr1 <darnelljr1@yahoo.com>
2025-12-31 19:12:31 -0500

    login validation setup added visAndEnabled utility function to testhelper added products page fixture

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -5,8 +5,9 @@ export class LoginPage extends BasePage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
-  readonly errorMessage: Locator;
   readonly titleSwagLabs: Locator;
+  readonly headerUsernames: Locator;
+  readonly headerPassword: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -14,8 +15,9 @@ export class LoginPage extends BasePage {
     this.usernameInput = page.locator('[data-test="username"]');
     this.passwordInput = page.locator('[data-test="password"]');
     this.loginButton = page.locator('[data-test="login-button"]');
-    this.errorMessage = page.locator('[data-test="error"]');
     this.titleSwagLabs = page.locator('.login_logo');
+    this.headerUsernames = page.getByRole('heading', { name: 'Accepted usernames are:' });
+    this.headerPassword = page.getByRole('heading', { name: 'Password for all users:' });
   }
 
   async goto() {
@@ -26,9 +28,5 @@ export class LoginPage extends BasePage {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
-  }
-
-  async expectLoginError() {
-    await expect(this.errorMessage).toBeVisible();
   }
 }
